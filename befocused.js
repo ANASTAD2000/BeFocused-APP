918ec662f70338f70f249da3770097cb88d6920f
anass taddart <anasbaclibr@gmail.com>
2025-01-30 00:23:00 +0100

    finish the html code (add pomodoro timer) and start in JS code

---
+++ befocused.js
@@ -0,0 +1,5 @@
+const taskForm = document.getElementById('taskForm');
+const taskTitle = document.getElementById('taskTitle');
+const taskDescription = document.getElementById('taskDescription');
+const taskPriority = document.getElementById('taskPriority');
+const taskList = document.getElementById('taskList');
